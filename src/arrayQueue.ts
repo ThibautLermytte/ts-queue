7d0c2ad68f135cbb482c59e45ea9a8c697949b3d
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-05-11 22:08:56 +0000

    Add array queue implementation

---
+++ src/arrayQueue.ts
@@ -0,0 +1,17 @@
+import Queue from './queue';
+
+export default class ArrayQueue<T> implements Queue<T> {
+  private data: Array<T> = [];
+
+  public enqueue(...datas: T[]): void {
+    this.data.push(...datas);
+  }
+
+  public dequeue(): T | undefined {
+    return this.data.shift();
+  }
+
+  public isEmpty(): boolean {
+    return this.data.length === 0;
+  }
+}
