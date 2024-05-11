9bf6f0e27479bebfa564c2a83817e74b7375276c
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-05-11 23:37:44 +0000

    Export lib

---
+++ index.ts
@@ -0,0 +1,6 @@
+import Queue from './lib/queue';
+import QueueDecorator from './lib/decorator';
+import ArrayQueue from './lib/implementations/arrayQueue';
+import LinkedListQueue from './lib/implementations/linkedListQueue';
+
+export { Queue, ArrayQueue, LinkedListQueue, QueueDecorator };
