bcd75cb6e639bd35646fb29e777b35c3d348dd29
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-05-11 22:53:49 +0000

    Add linked list queue test

---
+++ test/linkedListQueue.test.ts
@@ -0,0 +1,20 @@
+import { describe, test, expect } from 'vitest';
+import LinkedListQueue from '../src/linkedListQueue';
+
+const queue = new LinkedListQueue<number>();
+
+describe('Linked list queue', () => {
+  test('isEmpty', () => {
+    expect(queue.isEmpty()).toBeTruthy();
+  });
+  test('enqueue', () => {
+    queue.enqueue(0);
+    queue.enqueue(1);
+    expect(queue.isEmpty()).toBeFalsy();
+  });
+  test('dequeue', () => {
+    queue.dequeue();
+    queue.dequeue();
+    expect(queue.isEmpty()).toBeTruthy();
+  });
+});
