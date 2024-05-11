0e328853290ca8a2cd74d65a118870c64f82e624
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-05-11 23:25:10 +0000

    Add queue decorator

---
+++ lib/decorator.ts
@@ -0,0 +1,17 @@
+import Queue from './queue';
+
+export default class QueueDecorator<T = any> implements Queue<T> {
+  constructor(protected queue: Queue<T>) {}
+
+  public enqueue(...datas: T[]): void {
+    this.queue.enqueue(...datas);
+  }
+
+  public dequeue(): T | null | undefined {
+    return this.queue.dequeue();
+  }
+
+  public isEmpty(): boolean {
+    return this.queue.isEmpty();
+  }
+}
