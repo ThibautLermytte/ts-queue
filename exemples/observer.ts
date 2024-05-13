7503f5ae13e9c6c6cc5dc10b902d6292da02f80a
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-05-13 13:14:30 +0000

    Remove export instruction

--- exemples/observer.ts
+++ exemples/observer.ts
@@ -1,14 +1,11 @@
 import Queue from '../lib/queue';
 import QueueDecorator from '../lib/decorator';
 
-export interface Observer<T> {
+interface Observer<T> {
   update(...datas: T[]): void;
 }
 
-export default class QueueObserver<T>
-  extends QueueDecorator<T>
-  implements Observer<T>
-{
+class QueueObserver<T> extends QueueDecorator<T> implements Observer<T> {
   constructor(queue: Queue<T>) {
     super(queue);
   }
