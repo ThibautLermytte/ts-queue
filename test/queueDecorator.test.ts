f8a1c1c0c445208bdcaaf64498b2bfb62f439ef4
Thibaut LERMYTTE <lermytte.t@gmail.com>
2024-05-15 14:23:26 +0000

    Transform example into a unit test

---
+++ test/queueDecorator.test.ts
@@ -0,0 +1,28 @@
+import { describe, test, expect } from 'vitest';
+import ArrayQueue from '../lib/implementations/arrayQueue';
+import QueueDecorator from '../lib/decorator';
+import Queue from '../lib/queue';
+
+interface Observer<T> {
+  update(...datas: T[]): void;
+}
+
+class QueueObserver<T> extends QueueDecorator<T> implements Observer<T> {
+  constructor(queue: Queue<T>) {
+    super(queue);
+  }
+
+  public update(...datas: T[]): void {
+    this.enqueue(...datas);
+  }
+}
+
+const observer = new QueueObserver<number>(new ArrayQueue());
+
+describe('Queue decorator: observer pattern', () => {
+  test('update', () => {
+    observer.update(1);
+
+    expect(observer.isEmpty()).toBeFalsy();
+  });
+});
